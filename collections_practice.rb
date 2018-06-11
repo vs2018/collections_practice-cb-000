d1b178beed64bcafd1101b0cc6ba39182728a0f4
Vishal Sharma <vsh1819@gmail.com>
2018-06-11 08:44:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,105 @@
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort do |a,b|
+    if a == b
+      0 
+      elsif a < b
+      1 
+      elsif a > b
+      -1
+    end
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a,b|
+    if a.length == b.length
+      0 
+      elsif a.length > b.length
+      1 
+      elsif a.length < b.length
+      -1
+    end
+  end
+end
+
+def swap_elements(array)
+  
+  result = []
+  
+  array.each_with_index do |word,index|
+    if (index == 0)
+      result << word
+      elsif index == 2 
+      result << word
+    end
+  end
+  
+  result << array[1]
+  
+  return result
+
+  
+end
+
+
+def reverse_array(array)
+  
+  result = []
+  
+  counter = array.length
+  
+  while counter > 0
+    result << array[counter -1]
+    counter -= 1
+  end
+  
+  return result
+  
+end
+
+def kesha_maker(array)
+  
+  result = []
+  
+  array.each do |word|
+    new_array = word.split("")
+    new_array[2] = "$"
+    result << new_array.join
+  end
+  
+  return result
+end
+
+def find_a(array)
+  array.select do |word|
+    word.start_with?("a")
+  end
+  
+end
+
+def sum_array(array)
+  array.inject do |sum, n|
+    sum + n
+  end
+end
+
+def add_s(array)
+  result = []
+  
+  array.each_with_index do |word, index|
+    if index == 1 
+      result << word
+    else
+      new_word = word << "s"
+      result << new_word
+    end
+  end
+  
+  return result
+end
+
 
