421a9e88c6e50e3971bb307a3a6240059f04bcbd
Vishal Sharma <vsh1819@gmail.com>
2018-06-11 08:45:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -92,10 +92,10 @@ def add_s(array)
   
   array.each_with_index.collect do |word, index|
     if index == 1 
-      result << word
+      return word
     else
       new_word = word << "s"
-      result << new_word
+      return new_word
     end
   end
   
